ebaf9db587dd1fdce01017e69c756ed8074b7a74
Megymin <gerbera.uh@gmail.com>
2025-09-23 01:48:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 # Конфігурація бота
 # Вставте сюди токен вашого бота
 
-BOT_TOKEN = "8429770528:AAFvwwyry0k0473kEc1QZZlokblhXSMuz4Y"
+BOT_TOKEN = ""
 
 # Інші налаштування (за потреби)
 DEBUG = True
 LOG_LEVEL = "INFO"
+
